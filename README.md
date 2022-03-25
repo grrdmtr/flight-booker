df2d517192c919c2ccf41fad250d0ff3b46eaaf7
grrdmtr <dumitrugraur@gmail.com>
2022-03-25 18:26:11 +0200

    creates airport and flight models and controllers

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Flight-booker created with Ruby on Rails as part of The Odin Project curriculum.
 
-Things you may want to cover:
+This is a one-way flight booker. Practiced using populating and building dropdown menus, radio buttons, and nested submissions.
 
-* Ruby version
+* ruby 3.1.1
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+* Rails 7.0.2.3
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,18 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+  Airport.create(name: 'KIV')
+  Airport.create(name: 'OTP')
+  Airport.create(name: 'DUS')
+
+  Flight.create([
+                  { departure_airport_id: Airport.first.id, arrival_airport_id: Airport.last.id, duration: 320, start_date: '2022-12-25 07:00:00'},
+                  { departure_airport_id: Airport.first.id, arrival_airport_id: Airport.last.id, duration: 320, start_date: '2022-12-25 09:00:00'},
+                  { departure_airport_id: Airport.first.id, arrival_airport_id: Airport.last.id, duration: 320, start_date: '2022-12-25 11:00:00'},
+                  { departure_airport_id: Airport.last.id, arrival_airport_id: Airport.first.id, duration: 380, start_date: '2022-12-26 07:00:00'},
+                  { departure_airport_id: Airport.last.id, arrival_airport_id: Airport.first.id, duration: 380, start_date: '2022-12-26 09:00:00'},
+                  { departure_airport_id: Airport.last.id, arrival_airport_id: Airport.first.id, duration: 380, start_date: '2022-12-26 11:00:00'}
+                ])
